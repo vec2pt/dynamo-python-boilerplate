1ac2398c4895f80b2571415daeb74aa3b825b1c5
Vladyslav M <68401718+vlmarch@users.noreply.github.com>
2021-07-06 23:13:04 +0200

    Update PythonTemplate.py

--- PythonTemplate.py
+++ PythonTemplate.py
@@ -3,15 +3,15 @@ url = "https://github.com/vlmarch"
 
 import clr
 import sys
-sys.path.append('C:\Program Files (x86)\IronPython 2.7\Lib')
+# sys.path.append('C:\Program Files (x86)\IronPython 2.7\Lib')
 
 clr.AddReference('ProtoGeometry')
 from Autodesk.DesignScript.Geometry import *
 
 clr.AddReference("System")
-import System
+# import System
 from System import Array
-from System.Collections.Generic import List
+# from System.Collections.Generic import List
 
 clr.AddReference("RevitAPI")
 from Autodesk.Revit.DB import *
@@ -30,34 +30,14 @@ from RevitServices.Transactions import TransactionManager
 
 clr.AddReference("RevitNodes")
 import Revit
-clr.ImportExtensions(Revit.Elements)
-clr.ImportExtensions(Revit.GeometryConversion)
+clr.ImportExtensions(Revit.Elements) # ToDSType()
+clr.ImportExtensions(Revit.GeometryConversion) # ToProtoType(), ToRevitType()
 
 doc = DocumentManager.Instance.CurrentDBDocument
-uiapp = DocumentManager.Instance.CurrentUIApplication
+# uiapp = DocumentManager.Instance.CurrentUIApplication
 # app = uiapp.Application
-uidoc = uiapp.ActiveUIDocument
+# uidoc = uiapp.ActiveUIDocument
 
-# view = doc.ActiveView
-
-# Selected elements
-# elems_ids = uidoc.Selection.GetElementIds()
-# elems = []
-
-# for id in elems_ids:
-#	try: elems.append(doc.GetElement(id))
-#	except: elems.append(None)
-
-dataEnteringNode = IN
-
-if isinstance(IN[0],list): elems = UnwrapElement(IN[0])
-else: elems = [UnwrapElement(IN[0])]
-
-# TransactionManager.Instance.EnsureInTransaction(doc)
-# TransactionManager.Instance.TransactionTaskDone()
-
-# new_transaction = Transaction(doc)
-# new_transaction.Start("New Transaction")
-# new_transaction.Commit()
+elems = UnwrapElement(IN[0])
 
 OUT = elems
