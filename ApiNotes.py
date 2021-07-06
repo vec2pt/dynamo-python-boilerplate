eb9d1e22b203b98a2a8473c0e0344431416f944f
Vladyslav M <68401718+vlmarch@users.noreply.github.com>
2021-07-06 23:13:09 +0200

    Create ApiNotes.py

---
+++ ApiNotes.py
@@ -0,0 +1,19 @@
+# view = doc.ActiveView
+
+# Selected elements
+# elems_ids = uidoc.Selection.GetElementIds()
+# elems = []
+
+# for id in elems_ids:
+#	try: elems.append(doc.GetElement(id))
+#	except: elems.append(None)
+
+# if isinstance(IN[0],list): elems = UnwrapElement(IN[0])
+# else: elems = [UnwrapElement(IN[0])]
+
+# TransactionManager.Instance.EnsureInTransaction(doc)
+# TransactionManager.Instance.TransactionTaskDone()
+
+# new_transaction = Transaction(doc)
+# new_transaction.Start("New Transaction")
+# new_transaction.Commit()
