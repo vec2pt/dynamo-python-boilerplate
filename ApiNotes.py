2f628b8b3f336ef7713ebe5a8272ffa9ccee9006
Vladyslav M <68401718+vlmarch@users.noreply.github.com>
2021-07-07 22:18:00 +0200

    Update ApiNotes.py

--- ApiNotes.py
+++ ApiNotes.py
@@ -1,19 +1,53 @@
-# view = doc.ActiveView
+# Instance Elements Collector
+walls_inst = FilteredElementCollector(doc).OfClass(Wall).WhereElementIsNotElementType().ToElements()
+windows_inst = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Windows).WhereElementIsNotElementType().ToElements()
+
+# Type Elements Collector
+windows_types = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Windows).WhereElementIsElementType().ToElements()
+
+# Multicategory Collector
+categories_list = [BuiltInCategory.OST_Doors, BuiltInCategory.OST_Windows]
+categories = List[BuiltInCategory](categories_list)
+elem_filter = ElementMulticategoryFilter(categories)
+elems_inst = FilteredElementCollector(doc).WherePasses(elem_filter).WhereElementIsNotElementType().ToElements()
+
+# Get parameter value
+elem.LookupParameter("Keynote").AsString()
+elem.GetParameters("Keynote")[0].AsString()
+view.get_Parameter(BuiltInParameter.VIEW_TYPE).AsString()
+
+# Set parameter value
+elem.LookupParameter("Keynote").Set("xxx")
 
 # Selected elements
-# elems_ids = uidoc.Selection.GetElementIds()
-# elems = []
+uidoc.Selection.GetElementIds()
+[doc.GetElement(id) for id in uidoc.Selection.GetElementIds()]
 
-# for id in elems_ids:
-#	try: elems.append(doc.GetElement(id))
-#	except: elems.append(None)
+# Parameters
+elem.Parameters
+elem.GetOrderedParameters()
+elem.ParametersMap # elem.ParametersMap.Contains("Keynote")
 
-# if isinstance(IN[0],list): elems = UnwrapElement(IN[0])
-# else: elems = [UnwrapElement(IN[0])]
+# Active view
+view = doc.ActiveView
 
-# TransactionManager.Instance.EnsureInTransaction(doc)
-# TransactionManager.Instance.TransactionTaskDone()
+# Get by ID
+doc.GetElement(id)
 
-# new_transaction = Transaction(doc)
-# new_transaction.Start("New Transaction")
-# new_transaction.Commit()
+
+# Transaction - option 1
+TransactionManager.Instance.EnsureInTransaction(doc)
+TransactionManager.Instance.TransactionTaskDone()
+
+# Transaction - option 2
+new_transaction = Transaction(doc)
+new_transaction.Start("Transaction name")
+new_transaction.Commit()
+
+
+# pyRevit forms
+from pyrevit import forms
+
+# pyRevit doc
+doc = __revit__.ActiveUIDocument.Document
+# doc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument.Document
