57e6c3d79530afdbf2acddaa09f6606e63813d29
Vladyslav M <68401718+vlmarch@users.noreply.github.com>
2021-11-29 19:56:06 +0100

    Update PythonTemplate.py

--- PythonTemplate.py
+++ PythonTemplate.py
@@ -8,6 +8,9 @@ import sys
 clr.AddReference('ProtoGeometry')
 from Autodesk.DesignScript.Geometry import *
 
+# clr.AddReference('DSCoreNodes')
+# from DSCore import *
+
 clr.AddReference("System")
 # import System
 from System import Array
@@ -24,9 +27,6 @@ clr.AddReference("RevitServices")
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
 
-# clr.AddReference('DSCoreNodes')
-# from DSCore import *
-
 clr.AddReference("RevitNodes")
 import Revit
 clr.ImportExtensions(Revit.Elements) # ToDSType()
@@ -37,8 +37,58 @@ doc = DocumentManager.Instance.CurrentDBDocument
 # app = uiapp.Application
 # uidoc = uiapp.ActiveUIDocument
 
-# TransactionManager.Instance.EnsureInTransaction(doc)
-# TransactionManager.Instance.TransactionTaskDone()
+
+
+#############################################################################
+
+# Instance Elements Collector
+walls_inst = FilteredElementCollector(doc).OfClass(Wall).WhereElementIsNotElementType().ToElements()
+windows_inst = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Windows).WhereElementIsNotElementType().ToElements()
+
+# Type Elements Collector
+windows_types = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Windows).WhereElementIsElementType().ToElements()
+
+# Multicategory Collector
+categories_list = [BuiltInCategory.OST_Doors, BuiltInCategory.OST_Windows]
+categories = List[BuiltInCategory](categories_list)
+elem_filter = ElementMulticategoryFilter(categories)
+elems_inst = FilteredElementCollector(doc).WherePasses(elem_filter).WhereElementIsNotElementType().ToElements()
+
+# Get parameter value
+elem.LookupParameter("Keynote").AsString()
+elem.GetParameters("Keynote")[0].AsString()
+view.get_Parameter(BuiltInParameter.VIEW_TYPE).AsString()
+
+# Set parameter value
+elem.LookupParameter("Keynote").Set("xxx")
+
+# Selected elements
+uidoc.Selection.GetElementIds()
+[doc.GetElement(id) for id in uidoc.Selection.GetElementIds()]
+
+# Parameters
+elem.Parameters
+elem.GetOrderedParameters()
+elem.ParametersMap # elem.ParametersMap.Contains("Keynote")
+
+# Active view
+view = doc.ActiveView
+
+# Get by ID
+doc.GetElement(id)
+
+# Transaction - option 1
+TransactionManager.Instance.EnsureInTransaction(doc)
+TransactionManager.Instance.TransactionTaskDone()
+
+# Transaction - option 2
+new_transaction = Transaction(doc)
+new_transaction.Start("Transaction name")
+new_transaction.Commit()
+
+#############################################################################
+
+
 
 elems = UnwrapElement(IN[0])
 
